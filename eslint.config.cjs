c8246aa05ac6a9ec607520f35f3e0437047301f9
Dark-LYNN <botdevlynn@gmailcom>
2025-03-31 21:32:39 +0200

    refactor: changed config.

---
+++ eslint.config.cjs
@@ -0,0 +1,18 @@
+const js = require("@eslint/js");
+const ts = require("@typescript-eslint/eslint-plugin");
+const tsParser = require("@typescript-eslint/parser");
+
+module.exports = [
+  js.configs.recommended,
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tsParser
+    },
+    plugins: { "@typescript-eslint": ts },
+    rules: {
+      indent: ["error", 2, { SwitchCase: 1 }],
+      semi: ["error", "always"]
+    }
+  }
+];
