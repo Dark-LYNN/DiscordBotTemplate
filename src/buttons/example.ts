c19deb6f2a92db47ae9b35c1b286a1bc8b73fe75
Dark-LYNN <botdevlynn@gmailcom>
2025-03-31 21:39:15 +0200

    refactor: formatting

--- src/buttons/example.ts
+++ src/buttons/example.ts
@@ -4,6 +4,9 @@ import { ExtendedClient } from '@/types/extendedClient';
 export default {
   customId: 'example',
   async execute(client: ExtendedClient, interaction: ButtonInteraction) {
-    await interaction.reply({ content: 'You clicked the button!', flags: 'Ephemeral' });
+    await interaction.reply({
+      content: 'You clicked the button!',
+      flags: 'Ephemeral',
+    });
   },
 };
