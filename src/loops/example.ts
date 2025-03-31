24ea2297d52500d96515db4e3bfe819d650f2340
Dark-LYNN <botdevlynn@gmailcom>
2025-03-31 21:38:38 +0200

    refactor: semicolon

--- src/loops/example.ts
+++ src/loops/example.ts
@@ -3,8 +3,8 @@ import { logger } from '../utils';
 
 export default (client: ExtendedClient) => {
   const example = async () => {
-    logger.info('A loop ran.')
-  }
+    logger.info('A loop ran.');
+  };
 
   setInterval(() => example(), 30 * 60 * 1000); // runs example every 30 minutes (in ms)
 };
