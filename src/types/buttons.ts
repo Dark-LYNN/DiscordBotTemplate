96c84730b6ed830f3411d540c62d69653aef589b
Dark-LYNN <botdevlynn@gmailcom>
2025-03-31 21:33:05 +0200

    refactor: formatting

--- src/types/buttons.ts
+++ src/types/buttons.ts
@@ -1,7 +1,10 @@
-import { ButtonInteraction } from "discord.js";
+import { ButtonInteraction } from 'discord.js';
 import { ExtendedClient } from './extendedClient';
 
 export interface Button {
-  customId: string | ((id: string) => boolean)
-  execute: (client: ExtendedClient, interaction: ButtonInteraction) => Promise<void>;
+  customId: string | ((id: string) => boolean);
+  execute: (
+    client: ExtendedClient,
+    interaction: ButtonInteraction,
+  ) => Promise<void>;
 }
